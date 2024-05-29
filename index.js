9c7b9a387c8abf1ed852aaab7905994cc6208840
Alexei Baranov <alexei-baranov18@mail.ru>
2024-05-29 08:39:02 +0300

    update headers

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080;
 
-server.use(middlewares);
 
 server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Expose-Headers', 'X-Total-Count');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Expose-Headers', 'X-Total-Count, Content-Range');
     next();
 });
 
+server.use(middlewares);
 server.use(router);
 
 server.listen(port);
