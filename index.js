5076248545b6549d7715737255b173f2ee085ba9
Alexei Baranov <alexei-baranov18@mail.ru>
2024-05-29 08:08:32 +0300

    Added fn for see x-total-count

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080;
 
 server.use(middlewares);
+
+server.use((req, res, next) => {
+    res.header('Access-Control-Expose-Headers', 'X-Total-Count');
+    next();
+});
+
 server.use(router);
 
 server.listen(port);
